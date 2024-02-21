23871612f03e661c80af85d0ac2e78ee2ceb2820
hicham gaddari <gaddariowen@gmail.com>
2024-02-21 12:05:38 +0100

    add vercel SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from "@/components/ui/toaster";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] })
