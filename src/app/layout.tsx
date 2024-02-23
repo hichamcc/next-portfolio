32af87d63b23b82fdfd3de44ab7efb0d5f9a5afb
hicham gaddari <gaddariowen@gmail.com>
2024-02-23 10:29:46 +0100

    add SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Toaster />
-
+        <SpeedInsights />
       </body>
     </html>
   )
