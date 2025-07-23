3bac9bcc3a78aeb23ac74c83d5b2c440b550a2f9
hichamcc <gaddariowen@gmail.com>
2025-07-23 17:42:06 +0100

    correcting typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Code By Hicham',
-  description: 'My protfolio',
+  description: 'My portfolio',
 }
 
 export default function RootLayout({
